a9279d526d1ebb21a61974796a148d2571268f51
zomeze93 <111683260+zomeze93@users.noreply.github.com>
2025-03-06 21:47:10 +0100

    Update CharacterScript.cs
    add variables in work.

--- Astronaut - Programming Theory Project/Assets/Scripts/CharacterScript.cs
+++ Astronaut - Programming Theory Project/Assets/Scripts/CharacterScript.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
 {
+   
+    [Header ("Life")]
+    private float _life;
+    private float _maxLife;
+    //encapsulación
+    public float LifeValue()
+    {
+        get { return _life; }
+        ser { _life = value < 0 ? 0 : value; }
+    }
+
+    [Header ("Movement")]
+    private float _moveSpeed;
+    private float _mh;
+    private float _mv;
+    private RigidBody _rigid;
+    private Colider _col;
+    
     // Start is called before the first frame update
     void Start()
     {
+        _rigid = GetComponent<Rigidbody>();
+        _col = GetComponent<Colider>();
         
     }
 
@@ -15,4 +35,20 @@ public class CharacterScript : MonoBehaviour
     {
         
     }
+
+    void FixedUpdate()
+    {
+        _mh = Input.GetAxis("horizontal");
+        _mv = Input.GetAxis("vertical");
+        
+        MovePlayer(_mh,_mv);
+    }
+
+    public void MovePlayer(float mh, float mv)
+    {
+        _rigid.velocity = new Vector3(mh,0,mv) * _moveSpeed;
+    }
+
+    // abstraccion
+    //private void GetHit()
 }
