d1ea06ff29f3cd575fe3a5e6a205d11acb6e3ae7
zomeze93 <zomeze93@icloud.com>
2025-03-07 12:57:18 +0100

    add packings
    add two packages for the project.
    an astronaut and a star ship

--- Astronaut - Programming Theory Project/Assets/Scripts/CharacterScript.cs
+++ Astronaut - Programming Theory Project/Assets/Scripts/CharacterScript.cs
@@ -4,43 +4,43 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
 {
-   
+
     [Header ("Life")]
     private float _life;
     private float _maxLife;
     //encapsulación
-    public float LifeValue()
+    public float Life
     {
         get { return _life; }
-        ser { _life = value < 0 ? 0 : value; }
+        set { _life = value < 0 ? 0 : value; }
     }
 
     [Header ("Movement")]
     private float _moveSpeed;
     private float _mh;
     private float _mv;
-    private RigidBody _rigid;
-    private Colider _col;
-    
+    private Rigidbody _rigid;
+    private Collider _col;
+
     // Start is called before the first frame update
     void Start()
     {
         _rigid = GetComponent<Rigidbody>();
-        _col = GetComponent<Colider>();
-        
+        _col = GetComponent<Collider>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void FixedUpdate()
     {
         _mh = Input.GetAxis("horizontal");
         _mv = Input.GetAxis("vertical");
-        
+
         MovePlayer(_mh,_mv);
     }
 
